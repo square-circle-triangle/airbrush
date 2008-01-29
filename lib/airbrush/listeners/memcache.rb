bdf8112f6f3e323997557ff6185939c5df68b4aa
Marcus Crafter <crafterm@redartisan.com>
2008-01-29 04:17:29 +0000

    Added spec test for memcache listener and updated implementation.
    git-svn-id: svn://svn.sct.com.au/image_server/trunk@13 daa9635f-4444-0410-9b3d-b8c75a019348

--- lib/airbrush/listeners/memcache.rb
+++ lib/airbrush/listeners/memcache.rb
@@ -3,37 +3,50 @@ require 'memcache'
 module Airbrush
   module Listeners
     class Memcache < Listener
+      DEFAULT_POLL_FREQUENCY = 2 # seconds
+      DEFAULT_INCOMING_QUEUE = 'incoming'
+      
+      attr_reader :host, :poll_frequency
       
       def initialize
+        @poll_frequency = DEFAULT_POLL_FREQUENCY
         @host = '127.0.0.1:22122' # REVISIT: have to configure detection of memcache host (dnssd or command line option)
       end
     
       def start
         starling = MemCache.new(@host)
-      
+        
         loop do
-          op = starling.get('incoming')
-          
-          puts "op is #{op}"
-          
-          unless op
-            sleep 2 # REVISIT: configurable
-            next
-          end
-          
-          puts "handling op #{op}"
+          process(starling)
+          sleep @poll_frequency # REVISIT: configurable
+        end
+      end
+      
+      private
+      
+        def process(starling)
+          op = starling.get(DEFAULT_INCOMING_QUEUE)
+      
+          # command format from client  :command => :resize, :args => { :image => blob, :width => 300, :height => 200 }
+      
+          return unless op and valid?(op)
           
           begin
             @handler.process op[:command], op[:args]
-          rescue
+          rescue Exception => e
             # REVISIT: log bad command
+            puts "Recieved error during handler #{e}"
           end
-          
-          puts "#{op} handled"
+      
+        end
+        
+        def valid?(op)
+          return false unless op.is_a? Hash
+          return false unless op[:command]
+          return false unless op[:args]
+          true
         end
       
-        # command format from client  :command => :resize, :args => { :image => blob, :width => 300, :height => 200 }
-      end
     end
   end
 end
