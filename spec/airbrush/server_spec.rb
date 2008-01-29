4a3a78d2c89f2e6564de1890ead1f957fe3b6105
Marcus Crafter <crafterm@redartisan.com>
2008-01-29 00:35:02 +0000

    Add initial airbrush spec framework
    git-svn-id: svn://svn.sct.com.au/image_server/trunk@9 daa9635f-4444-0410-9b3d-b8c75a019348

---
+++ spec/airbrush/server_spec.rb
@@ -0,0 +1,19 @@
+require File.dirname(__FILE__) + '/../spec_helper.rb'
+
+describe Airbrush::Server, 'when created' do
+  
+  it 'should automatically create a listener for incoming requests'
+  
+  it 'should automatically create a handler to handle incoming requests'
+
+  it 'should automatically configure the handler with a processor'
+  
+  it 'should automatically configure the handler with a publisher'
+  
+end
+
+describe Airbrush::Server, 'when started' do
+  
+  it 'should start accepting incoming requests via the listener'
+  
+end
