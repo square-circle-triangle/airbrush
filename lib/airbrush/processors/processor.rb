9bb27f8a0bf3cc519b1abc2512cdb33f15864284
Marcus Crafter <crafterm@redartisan.com>
2008-01-29 05:12:05 +0000

    Update processor to use a dedicated dispatch method rather than method missing. Update dispatch to use Merb's get_args extension to determine the names of the target action, extract them from the parameters hash and pass them through to the action signature automatically (requires ruby2ruby). Updated spec test to validate.
    git-svn-id: svn://svn.sct.com.au/image_server/trunk@20 daa9635f-4444-0410-9b3d-b8c75a019348

--- lib/airbrush/processors/processor.rb
+++ lib/airbrush/processors/processor.rb
@@ -1,9 +1,26 @@
+require 'airbrush/core_ext/get_args'
+
 module Airbrush
   module Processors
     class Processor
-      def method_missing(sym, args, &block)
-        raise "Unknown processor operation #{sym} (#{args.inspect unless args.blank?})"
+      
+      def dispatch(command, args)
+        raise "Unknown processor operation #{sym} (#{args.inspect unless args.blank?})" unless respond_to? command
+        
+        params = assign(command, args)
+        self.send command, *params
       end
+            
+      private
+      
+        def assign(command, args)
+          params = self.class.instance_method(command).get_args
+          params.collect do |param|
+            name, default = *param
+            args[name] ? args[name] : (raise "No value (default or otherwise) provided for #{name}" unless default; default)            
+          end
+        end
+      
     end
   end
 end
--- spec/airbrush/processors/processor_spec.rb
+++ spec/airbrush/processors/processor_spec.rb
@@ -1,13 +1,55 @@
 require File.dirname(__FILE__) + '/../../spec_helper.rb'
 
-describe Airbrush::Processors::Processor, 'when processing' do
+class MyProcessor < Airbrush::Processors::Processor
+  
+  def my_action(one, two, three = '3')
+    [ one, two, three ]
+  end
+
+end
+
+describe Airbrush::Processors::Processor, 'abstract class' do
   
   before do
     @processor = Airbrush::Processors::Processor.new
   end
   
-  it 'should raise an error upon receiving method calls, since all operations are to be implemented in concrete classes' do
-    lambda { @processor.send :blah }.should raise_error
+  it 'should rasie an error from dispatch if the requested command is not implemented by the processor' do
+    lambda { @processor.dispatch(:blah, {}) }.should raise_error
+  end
+  
+end
+
+describe Airbrush::Processors::Processor, 'argument handling' do
+  
+  before do
+    @processor = MyProcessor.new
+  end
+  
+  it 'should inspect the target action for its parameters and assign them via the argument hash' do
+    params = @processor.send :assign, :my_action, { :one => 'a', :two => 'b', :three => 'c' }
+    params.should == %w( a b c )
   end
 
+  it 'should respect default values when present in action signatures, but not in the argument hash' do
+    params = @processor.send :assign, :my_action, { :one => 'a', :two => 'b' }
+    params.should == %w( a b 3 )
+  end
+  
+  it 'should raise an error if an action signature value is missing from the argument hash and does not have a default value' do
+    lambda { @processor.send(:assign, :my_action, { :one => 'a' }) }.should raise_error
+  end
+  
+end
+
+describe Airbrush::Processors::Processor, 'when processing' do
+  
+  before do
+    @processor = MyProcessor.new
+  end
+  
+  it 'should invoke the requested target action' do
+    @processor.dispatch(:my_action, { :one => 'a', :two => 'b' }).should == %w( a b 3 )
+  end
+  
 end
