e70fa89092757aa2919144cade030cdd8756b89e
Marcus Crafter <crafterm@redartisan.com>
2008-01-29 07:18:10 +0000

    Update use of RMagick API to read the image data from a blob since the content comes via arguments and not a local file
    git-svn-id: svn://svn.sct.com.au/image_server/trunk@33 daa9635f-4444-0410-9b3d-b8c75a019348

--- lib/airbrush/processors/rmagick.rb
+++ lib/airbrush/processors/rmagick.rb
@@ -4,7 +4,7 @@ module Airbrush
   module Processors
     class Rmagick < Processor
       def resize(image, width, height)
-        img = Magick::Image.read(image)
+        img = Magick::Image.from_blob(image).first
         img.change_geometry("#{width}x#{height}") { |cols, rows, image| img.resize!(cols, rows) }
         img.to_blob
       end
