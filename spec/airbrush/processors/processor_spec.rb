bdc34e2fbe8da4859bd07c24f5b1175f0b7ee947
Marcus Crafter <crafterm@redartisan.com>
2008-01-29 05:12:02 +0000

    Add spec to enure that all method calls raise errors in abstract processor class
    git-svn-id: svn://svn.sct.com.au/image_server/trunk@17 daa9635f-4444-0410-9b3d-b8c75a019348

--- spec/airbrush/processors/processor_spec.rb
+++ spec/airbrush/processors/processor_spec.rb
@@ -1,5 +1,13 @@
 require File.dirname(__FILE__) + '/../../spec_helper.rb'
 
-describe Airbrush::Processors::Processor, 'when created' do
+describe Airbrush::Processors::Processor, 'when processing' do
   
+  before do
+    @processor = Airbrush::Processors::Processor.new
+  end
+  
+  it 'should raise an error upon receiving method calls, since all operations are to be implemented in concrete classes' do
+    lambda { @processor.send :blah }.should raise_error
+  end
+
 end
