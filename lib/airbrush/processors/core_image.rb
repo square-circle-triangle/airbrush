676135c0e83b0849e74fe785af62b4479e868568
Marcus Crafter <crafterm@redartisan.com>
2008-02-11 04:56:33 +0000

    Remove core image processor until API has stabilized
    git-svn-id: svn://svn.sct.com.au/image_server/trunk@45 daa9635f-4444-0410-9b3d-b8c75a019348

--- lib/airbrush/processors/core_image.rb
+++
@@ -1,9 +0,0 @@
-module Airbrush
-  module Processors
-    class CoreImage < Processor
-      def resize(image, width, height) # lets do it the merb way?
-        # resize the image and return a copy of it inside of ruby
-      end
-    end
-  end
-end
