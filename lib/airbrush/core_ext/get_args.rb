4dac1db451f44cfe4bcf55d254b5111ccbef1cb2
Marcus Crafter <crafterm@redartisan.com>
2008-07-01 03:20:07 +0000

    Update get_args to latest version and explicitly require parsetree, required after gem update
    git-svn-id: svn://svn.sct.com.au/image_server/trunk@78 daa9635f-4444-0410-9b3d-b8c75a019348

--- lib/airbrush/core_ext/get_args.rb
+++ lib/airbrush/core_ext/get_args.rb
@@ -1,78 +1,94 @@
-# From get_args.rb in Merb under the MIT-LICENSE, thanks guys
+require 'ruby2ruby'
+require 'parse_tree'
 
-begin
-  require 'ruby2ruby'
-  
-  class ParseTreeArray < Array #:nodoc:
-    def self.translate(*args)
-      self.new(ParseTree.translate(*args))
-    end
-  
-    def deep_array_node(type = nil)
-      each do |node|
-        return ParseTreeArray.new(node) if node.is_a?(Array) && (!type || node[0] == type)
-        next unless node.is_a?(Array)
-        return ParseTreeArray.new(node).deep_array_node(type)
+class ParseTreeArray < Array #:nodoc:
+  def self.translate(*args)
+    sexp = ::ParseTree.translate(*args)
+    # ParseTree.translate returns [nil] if called on an inherited method, so walk
+    # up the inheritance chain to find the class that the method was defined in
+    unless sexp.first
+      klass = args.first.ancestors.detect do |klass|
+        klass.public_instance_methods(false).include?(args.last.to_s)
       end
-      nil
+      sexp = ::ParseTree.translate(klass, args.last) if klass
     end
-  
-    def arg_nodes
-      self[1..-1].inject([]) do |sum,item|
-        sum << [item] unless item.is_a?(Array)
-        sum
-      end
+    self.new(sexp)
+  end
+
+  def deep_array_node(type = nil)
+    each do |node|
+      return ParseTreeArray.new(node) if node.is_a?(Array) && (!type || node[0] == type)
+      next unless node.is_a?(Array)
+      return ParseTreeArray.new(node).deep_array_node(type)
     end
-    
-    def get_args
-      arg_node = deep_array_node(:args)
-      return nil unless arg_node
+    nil
+  end
+
+  def arg_nodes
+    self[1..-1].inject([]) do |sum,item|
+      sum << [item] unless item.is_a?(Array)
+      sum
+    end
+  end
+
+  def get_args
+    if arg_node = deep_array_node(:args)
+      # method defined with def keyword
       args = arg_node.arg_nodes
       default_node = arg_node.deep_array_node(:block)
       return args unless default_node
-      lasgns = default_node[1..-1]
-      lasgns.each do |asgn|
-        args.assoc(asgn[1]) << eval(RubyToRuby.new.process(asgn[2]))
-      end
-      args
+    else
+      # assuming method defined with Module#define_method
+      return []
     end
-  
-  end
 
-  # Used in mapping controller arguments to the params hash.
-  # NOTE: You must have the 'ruby2ruby' gem installed for this to work.
-  # Example:
-  #   (In PostsController)
-  #   def show(id)  #=> id is the same as params[:id]
+    # if it was defined with def, and we found the default_node,
+    # that should bring us back to regularly scheduled programming..
 
-  module GetArgs
-    
-    # Returns an array of method arguments and their default values
-    # Example:
-    #   class Example
-    #     def hello(one,two="two",three)
-    #     end
-    #
-    #     def goodbye
-    #     end
-    #   end
-    #
-    #   Example.instance_method(:hello).get_args    #=> [[:one], [:two, "two"], [:three, "three"]]
-    #   Example.instance_method(:goodbye).get_args  #=> nil
-    def get_args
-      klass, meth = self.to_s.split(/ /).to_a[1][0..-2].split("#")
-      # Remove stupidity for #<Method: Class(Object)#foo>
-      klass = $` if klass =~ /\(/
-      ParseTreeArray.translate(Object.const_get(klass), meth).get_args
+    lasgns = default_node[1..-1]
+    lasgns.each do |asgn|
+      args.assoc(asgn[1]) << eval(RubyToRuby.new.process(asgn[2]))
     end
+    args
   end
 
-  class UnboundMethod #:nodoc:
-    include GetArgs
-  end
+end
 
-  class Method  #:nodoc:
-    include GetArgs
+# Used in mapping controller arguments to the params hash.
+# NOTE: You must have the 'ruby2ruby' gem installed for this to work.
+#
+# ==== Examples
+#   # In PostsController
+#   def show(id)  #=> id is the same as params[:id]
+module GetArgs
+
+  # ==== Returns
+  # Array:: Method arguments and their default values.
+  #
+  # ==== Examples
+  #   class Example
+  #     def hello(one,two="two",three)
+  #     end
+  #
+  #     def goodbye
+  #     end
+  #   end
+  #
+  #   Example.instance_method(:hello).get_args
+  #     #=> [[:one], [:two, "two"], [:three, "three"]]
+  #   Example.instance_method(:goodbye).get_args  #=> nil
+  def get_args
+    klass, meth = self.to_s.split(/ /).to_a[1][0..-2].split("#")
+    # Remove stupidity for #<Method: Class(Object)#foo>
+    klass = $` if klass =~ /\(/
+    ParseTreeArray.translate(Object.const_get(klass), meth).get_args
   end
-rescue LoadError
+end
+
+class UnboundMethod #:nodoc:
+  include GetArgs
+end
+
+class Method  #:nodoc:
+  include GetArgs
 end
