45a5ff49554f38b64bcd9d8e93ebd36f989c1231
Marcus Crafter <crafterm@redartisan.com>
2008-07-01 03:20:15 +0000

    Update specs following minor code tweaks
    git-svn-id: svn://svn.sct.com.au/image_server/trunk@82 daa9635f-4444-0410-9b3d-b8c75a019348

--- spec/airbrush/client_spec.rb
+++ spec/airbrush/client_spec.rb
@@ -2,7 +2,7 @@ require File.dirname(__FILE__) + '/../spec_helper.rb'
 require 'starling'
 
 describe Airbrush::Client, 'job management' do
-  
+
   before do
     @id = :id
     @command = :command
@@ -11,18 +11,18 @@ describe Airbrush::Client, 'job management' do
     @queue = 'incoming'
     @response_timeout = 5.minutes
     @queue_validity = 15.minutes
-    
+
     @server = mock(Starling)
     @server.stub!(:set).and_return
     @server.stub!(:get).and_return('results')
     Starling.stub!(:new).and_return(@server)
-    
+
     @host = 'host'
     @client = Airbrush::Client.new(@host, @queue, @response_timeout, @queue_validity)
   end
-  
+
   describe Airbrush::Client, 'when created' do
-    
+
     it 'should support a configurable target memcache host' do
       @client.host.should == @host
     end
@@ -30,7 +30,7 @@ describe Airbrush::Client, 'job management' do
     it 'should support a configurable inbound queue name' do
       @client.incoming_queue.should == @queue
     end
-    
+
     it 'should support a configurable target memcache host' do
       @client.response_timeout.should == @response_timeout
     end
@@ -38,50 +38,50 @@ describe Airbrush::Client, 'job management' do
     it 'should support a configurable queue validity' do
       @client.queue_validity.should == @queue_validity
     end
-    
+
   end
-  
+
   describe Airbrush::Client, 'job management' do
 
     it 'should allow a job to be posted' do
       @client.should respond_to(:process)
     end
-  
+
     it 'should allow access to the results returned by a job' do
       @client.process(@id, @command, @args).should == @results
     end
-    
+
   end
-  
+
   describe Airbrush::Client, 'a posted job' do
-    
+
     it 'should raise an error if the id is not valid' do
       lambda { @client.process(nil, @command, @args) }.should raise_error
     end
-  
+
     it 'should raise an error if the command is not valid' do
       lambda { @client.process(@id, nil, @args) }.should raise_error
     end
-  
+
     it 'should raise an error if the args are not valid' do
       lambda { @client.process(@id, @command, nil) }.should raise_error
     end
-  
+
     it 'should support optional arguments' do
       lambda { @client.process(@id, @command) }.should_not raise_error
     end
-    
+
     it 'should time out after 2 minutes seconds of inactivity' do
       Airbrush::Client::DEFAULT_RESPONSE_TIMEOUT.should == 2.minutes
       Timeout.should_receive(:timeout).and_return
       @client.process(@id, @command)
     end
-    
+
     it 'should add the job into the incoming process queue' do
-      @server.should_receive(:set).with(@queue, { :id => @id, :command => @command, :args => @args }, @queue_validity)
+      @server.should_receive(:set).with(@queue, { :id => @id, :command => @command, :args => @args }, @queue_validity, false)
       @client.process(@id, @command, @args)
     end
-    
+
   end
-  
+
 end
--- spec/airbrush/processors/image/rmagick_spec.rb
+++ spec/airbrush/processors/image/rmagick_spec.rb
@@ -2,24 +2,25 @@ require File.dirname(__FILE__) + '/../../../spec_helper.rb'
 require 'RMagick'
 
 describe Airbrush::Processors::Image::Rmagick, 'class' do
-  
+
   before do
     @image = 'image'
-  
+
     @rm_image = mock(Object)
     @rm_image.stub!(:change_geometry).and_return
     @rm_image.stub!(:crop!).and_return
     @rm_image.stub!(:crop_resized!).and_return
     @rm_image.stub!(:ensure_rgb!).and_return
     @rm_image.stub!(:to_blob).and_return('blob')
-  
+    @rm_image.stub!(:format=).and_return('JPEG')
+
     @processor = Airbrush::Processors::Image::Rmagick.new
     @processor.stub!(:purify_image).and_return
     Magick::Image.stub!(:from_blob).and_return([@rm_image])
   end
-  
+
   describe Airbrush::Processors::Image::Rmagick, 'when resizing' do
-  
+
     it 'should preprocess images before resizing' do
       @processor.resize @image, 300, 200
     end
@@ -28,7 +29,7 @@ describe Airbrush::Processors::Image::Rmagick, 'class' do
       @rm_image.should_receive(:change_geometry).and_return
       @processor.resize @image, 300, 200
     end
-    
+
     it 'should convert images to rgb if required' do
       @rm_image.should_receive(:ensure_rgb!).and_return
       @processor.resize @image, 300, 200
@@ -38,15 +39,15 @@ describe Airbrush::Processors::Image::Rmagick, 'class' do
       @rm_image.should_receive(:to_blob).and_return('blob')
       @processor.resize(@image, 300, 200).should == 'blob'
     end
-  
+
   end
 
   describe Airbrush::Processors::Image::Rmagick, 'when cropping' do
-  
+
     it 'should preprocess images before cropping' do
       @processor.crop @image, 10, 10, 100, 100
     end
-  
+
     it 'should change the geometry of the image' do
       @rm_image.should_receive(:crop!).and_return
       @processor.crop @image, 10, 10, 100, 100
@@ -61,20 +62,20 @@ describe Airbrush::Processors::Image::Rmagick, 'class' do
       @rm_image.should_receive(:to_blob).and_return('blob')
       @processor.crop(@image, 10, 10, 100, 100).should == 'blob'
     end
-  
+
   end
 
   describe Airbrush::Processors::Image::Rmagick, 'when performing a resized crop' do
-  
+
     it 'should preprocess images before resizing/cropping' do
       @processor.crop_resize @image, 75, 75
     end
-  
+
     it 'should change the geometry of the image' do
       @rm_image.should_receive(:crop_resized!).and_return
       @processor.crop_resize @image, 75, 75
     end
-    
+
     it 'should convert images to rgb if required' do
       @rm_image.should_receive(:ensure_rgb!).and_return
       @processor.resize @image, 300, 200
@@ -84,11 +85,11 @@ describe Airbrush::Processors::Image::Rmagick, 'class' do
       @rm_image.should_receive(:to_blob).and_return('blob')
       @processor.crop_resize(@image, 75, 75).should == 'blob'
     end
-  
+
   end
 
   describe Airbrush::Processors::Image::Rmagick, 'when generating previews' do
-  
+
     it 'should change the geometry of the image' do
       @rm_image.should_receive(:crop_resized!).twice.and_return
       @processor.previews @image, { :small => [200,100], :large => [500,250] }
@@ -98,54 +99,53 @@ describe Airbrush::Processors::Image::Rmagick, 'class' do
       @rm_image.should_receive(:to_blob).twice.and_return('blob')
       @processor.previews(@image, { :small => [200,100], :large => [500,250] }).should == { :small => 'blob', :large => 'blob'}
     end
-  
+
   end
 
 end
 
 describe Magick::Image, 'when created' do
-  
+
   before do
     @image = Magick::Image.new(200, 100)
   end
-  
+
   it 'should be able to convert the profile to srgb when requested' do
     @image.should respond_to(:ensure_rgb!)
   end
-  
+
 end
 
 describe Magick::Image, 'when converting images to sRGB' do
-  
+
   before do
     @image = Magick::Image.new(200, 100)
     @image.stub!(:add_profile).and_return
   end
-  
+
   it 'should return if the image is already in sRGB colour space' do
     @image.colorspace = Magick::RGBColorspace
     @image.should_not_receive(:add_profile)
     @image.ensure_rgb!
   end
-  
+
   it 'should log a warning if a non CMYK/RGB image is encountered' do
     @image.colorspace = Magick::Rec709LumaColorspace
     @image.should_not_receive(:add_profile)
     @image.log.should_receive(:warn).and_return
     @image.ensure_rgb!
   end
-  
+
   it 'should add a CMYK profile if the image does not have a profile and is in CMYK colour space' do
     @image.colorspace = Magick::CMYKColorspace
     @image.should_receive(:add_profile).twice.and_return
     @image.ensure_rgb!
   end
-  
+
   it 'should add a sRGB profile if the image is in CMYK colour space' do
     @image.colorspace = Magick::CMYKColorspace
     @image.should_receive(:add_profile).with(Magick::Image::SCT_SRGB_ICC).and_return
     @image.ensure_rgb!
   end
-  
-end
 
+end
