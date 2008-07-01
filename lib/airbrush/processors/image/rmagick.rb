8ed43fde5cee208ad66dd71627df8383f59afecc
Marcus Crafter <crafterm@redartisan.com>
2008-07-01 03:20:09 +0000

    Exception text update to make inform developer where to add code
    git-svn-id: svn://svn.sct.com.au/image_server/trunk@79 daa9635f-4444-0410-9b3d-b8c75a019348

--- lib/airbrush/processors/image/rmagick.rb
+++ lib/airbrush/processors/image/rmagick.rb
@@ -4,7 +4,7 @@ module Airbrush
   module Processors
     module Image
       class Rmagick < ImageProcessor
-        
+
         def resize(image, width, height)
           img = load(image)
           img.change_geometry("#{width}x#{height}") { |cols, rows, image| img.resize!(cols, rows) }
@@ -12,7 +12,7 @@ module Airbrush
           img.format = 'JPEG'
           img.to_blob
         end
-        
+
         def crop(image, tl_x, tl_y, br_x, br_y)
           img = load(image)
           img.crop!(tl_x, tl_y, br_x, br_y)
@@ -28,17 +28,17 @@ module Airbrush
           img.format = 'JPEG'
           img.to_blob
         end
-        
+
         def previews(image, sizes) # sizes => { :small => [200,100], :medium => [400,200], :large => [600,300] }
           sizes.inject(Hash.new) { |m, (k, v)| m[k] = crop_resize(image, *v); m }
         end
-        
+
         protected
-        
+
           def load(image)
             img = Magick::Image.from_blob(image).first
           end
-        
+
       end
     end
   end
@@ -48,25 +48,25 @@ module Magick
   class Image
     SCT_CMYK_ICC = File.dirname(__FILE__) + '/profiles/cmyk-profile.icc'
     SCT_SRGB_ICC = File.dirname(__FILE__) + '/profiles/srgb-profile.icc'
-    
+
     def ensure_rgb!
       return if rgb?
-      
+
       if cmyk?
         add_profile SCT_CMYK_ICC if color_profile == nil
         add_profile SCT_SRGB_ICC
         log.debug "Added sRGB profile to image"
       else
-        log.warn "Non CMYK/RGB color profile encountered, please install a profile for #{colorspace}"
+        log.warn "Non CMYK/RGB color profile encountered, please install a profile for #{colorspace} and update ensure_rgb! implementation"
       end
     end
-    
+
     private
-    
+
       def rgb?
         colorspace == Magick::RGBColorspace
       end
-      
+
       def cmyk?
         colorspace == Magick::CMYKColorspace
       end
