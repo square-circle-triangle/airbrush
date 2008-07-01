262e24d517eb4f78d58596cf8356c04e6e4d457a
Marcus Crafter <crafterm@redartisan.com>
2008-07-01 05:24:22 +0000

    Whitespace formatting update
    git-svn-id: svn://svn.sct.com.au/image_server/trunk@84 daa9635f-4444-0410-9b3d-b8c75a019348

--- lib/airbrush/listeners/memcache.rb
+++ lib/airbrush/listeners/memcache.rb
@@ -5,40 +5,40 @@ module Airbrush
     class Memcache < Listener
       DEFAULT_POLL_FREQUENCY = 2 # seconds
       DEFAULT_INCOMING_QUEUE = 'airbrush_incoming_queue'
-      
+
       attr_reader :host, :poll_frequency
-      
+
       def initialize(host, frequency = DEFAULT_POLL_FREQUENCY)
         @host = host
         @poll_frequency = frequency
         catch_signals(:int)
       end
-    
+
       def start
         @running = true
         @starling = MemCache.new(@host)
-        
+
         log.debug 'Accepting incoming jobs'
-        
+
         loop do
           poll do |operation|
             process operation
           end
-          
+
           break unless @running
           sleep @poll_frequency
         end
-        
+
         @starling.reset
       end
-      
+
       private
-      
+
         def poll
           operation = @starling.get(DEFAULT_INCOMING_QUEUE)
           yield operation if operation and block_given?
         end
-        
+
         def catch_signals(*signals)
           signals.each do |signal|
             sig = signal.to_s.upcase
